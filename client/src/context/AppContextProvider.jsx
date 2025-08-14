8d4061326faa6f71ec116769042c933d63d040c2
Koushik Guria <koushikguria998@gmail.com>
2025-08-14 17:03:04 +0530

    Initial commit & Navbar added

---
+++ client/src/context/AppContextProvider.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { AppContext } from "./AppContext";
+import { useNavigate } from "react-router-dom";
+
+export const AppContextProvider = ({children}) => {
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(true);
+    const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+
+    const value = {
+        navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin
+    };
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+    
+}
