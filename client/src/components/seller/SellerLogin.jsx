74a2825fba1c2e3ed146fb11d049d01d68b4c001
Koushik Guria <koushikguria998@gmail.com>
2025-09-14 21:35:11 +0530

    Seller/admin pages added

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -26,7 +26,9 @@ const SellerLogin = () => {
 
             <div className="w-full">
                 <p>Email</p>
-                <input className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+                <input onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
                 type="email" 
                 placeholder="Enter email"
                 required
@@ -34,7 +36,9 @@ const SellerLogin = () => {
             </div>
             <div className="w-full">
                 <p>Password</p>
-                <input className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+                <input onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
                 type="password" 
                 placeholder="Enter password"
                 required
