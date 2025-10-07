0e8303bea1e72cc6fd0f4edd728e71a4f12a8144
Koushik Guria <koushikguria998@gmail.com>
2025-10-07 22:07:48 +0530

    userController api added

---
+++ server/controllers/sellerController.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+// Login Seller: /api/seller/login
+export const sellerLogin = async (req, res) => {
+    const { email, password } = req.body;
+
+    if(password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL) {
+        const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: "7d"});
+
+        res.cookie("sellerToken", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            maxAge: 7*24*60*60*1000
+        });
+
+        return res.json({success: true, message: "Logged In"});
+    }
+}
